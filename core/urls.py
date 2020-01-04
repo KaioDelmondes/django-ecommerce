b6faa8d3faf0a2b0af1f15e2675a0cc242f23f9c
KaioDelmondes <kaioregodearaujo@hotmail.com>
2020-01-04 20:52:51 -0300

    Criando um resumo de compra.

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,9 @@ app_name = 'core'
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
     path('checkout/', views.checkout, name='checkout'),
+    path('order-sumary/', views.OrderSumaryView.as_view(), name='order-sumary'),
     path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<slug>/', views.remove_from_cart, name='remove-from-cart'),
+    path('remove-single-item-from-cart/<slug>/', views.remove_single_item_from_cart, name='remove-single-item-from-cart')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from .models import Item, Order, OrderItem
 
@@ -20,6 +23,19 @@ class ItemDetailView(DetailView):
     model = Item
     template_name = "product-page.html"
 
+class OrderSumaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user = self.request.user, ordered = False)
+            context={
+                'object': order
+                }
+            return render(self.request, 'order-sumary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active error")
+            return redirect("/")
+
+@login_required            
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug = slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -35,18 +51,19 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated.")
-            return redirect('core:product', slug = slug)
+            return redirect('core:order-sumary')
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart.")
-            return redirect('core:product', slug = slug)
+            return redirect('core:order-sumary')
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(user = request.user, ordered_date = ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect('core:product', slug = slug)
+        return redirect('core:order-sumary')
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug = slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
@@ -68,5 +85,30 @@ def remove_from_cart(request, slug):
     else:
         messages.info(request, "You do not have an active order")
         return redirect('core:product', slug = slug)
-    
-    
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug = slug)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug = item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user = request.user,
+                ordered = False
+                )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect('core:order-sumary')
+        else:
+            messages.info(request, "This item was not in your cart.")
+            return redirect('core:product', slug = slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect('core:product', slug = slug)
